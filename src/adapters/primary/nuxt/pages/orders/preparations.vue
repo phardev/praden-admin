7e8b986057ce6cc0d7ea4a148fff957192d56a5f
gmevelec <gregory.mevelec@gmail.com>
2023-02-14 02:18:22 +0100

    Allow to print with qrcode

--- src/adapters/primary/nuxt/pages/orders/preparations.vue
+++ src/adapters/primary/nuxt/pages/orders/preparations.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
 .section
-  fv-table(
+  div.hidden.printme(id="to-print")
+    p Récapitulatif des commandes
+    fv-table(
+      :headers="[{ name: 'Référence', value: 'reference'}, {name: 'Nom', value: 'name'}, {name: 'Quantité', value: 'quantity'}]"
+      :items="startVM.global"
+    )
+    div.break-before-page(v-for="order in startVM.detail" :key="order.reference")
+      div.flex
+        h1.text-xl.grow Commande {{ order.reference }}
+        vueQr(:text="order.href")
+      fv-table(
+        :headers="[{ name: 'Référence', value: 'reference'}, {name: 'Nom', value: 'name'}, {name: 'Quantité', value: 'quantity'}]"
+        :items="order.lines"
+      )
+  fv-table.no-printme(
     :headers="ordersVM.headers"
     :items="ordersVM.items"
     :selectable="true"
@@ -13,9 +27,10 @@
       .font-medium.text-default {{ item.reference }}
     template(#createdDate="{ item }")
       time(:datetime='item.createdDatetime') {{ item.createdDate }}
-  div.w-full.flex.flex-row-reverse
+  div.w-full.flex.flex-row-reverse.no-printme
     ft-button.button-solid.mt-4.mr-0.py-4.px-4.text-xl(
       v-if="ordersSelectedVM.items.length > 0"
+      @click="startPreparations"
     ) Commencer les préparations
 </template>
 
@@ -28,6 +43,14 @@ import { toggleSelectPreparation } from '@core/usecases/order/toggle-select-prep
 import { toggleSelectAllPreparations } from '@core/usecases/order/toggle-select-all-preparations/toggleSelectAllPreparations'
 import FtButton from '@adapters/primary/nuxt/components/FtButton.vue'
 import { startPreparationsVM } from '@adapters/primary/view-models/start-preparations/startPreparationsVM'
+import isBrowser from 'is-browser'
+
+let vueQr
+if (isBrowser) {
+  import('vue-qr/src/packages/vue-qr.vue').then((module) => {
+    vueQr = module.default
+  })
+}
 
 onMounted(() => {
   listOrdersToPrepare(useOrderGateway())
@@ -41,11 +64,19 @@ const ordersSelectedVM = computed(() => {
   return getSelectedPreparationsVM()
 })
 
+const startVM = computed(() => {
+  return startPreparationsVM()
+})
+
 const select = (selected: any) => {
   toggleSelectPreparation(selected.reference)
 }
 
 const selectAll = () => {
   toggleSelectAllPreparations()
+}
+
+const startPreparations = () => {
+  window.print()
 }
 </script>
