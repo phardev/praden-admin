bc49c1a6fde80dca0ea6abe150528052ee7b5a45
gmevelec <gregory.mevelec@gmail.com>
2023-02-19 20:06:11 +0100

    Add real date provider

--- gateways/orderGateway.ts
+++ gateways/orderGateway.ts
@@ -1,8 +1,9 @@
 import { InMemoryOrderGateway } from '@adapters/secondary/inMemoryOrderGateway'
 import * as orders from '@utils/testData/orders'
+import { RealDateProvider } from '@adapters/secondary/realDateProvider'
 
 export const useOrderGateway = () => {
-  const orderGateway = new InMemoryOrderGateway()
+  const orderGateway = new InMemoryOrderGateway(new RealDateProvider())
   orderGateway.feedWith(...Object.values(orders))
   return orderGateway
 }
