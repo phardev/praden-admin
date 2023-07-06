aa08fa1502410826c358b47f46d44a30e093f06e
ChocoDev23 <chocowalletdev@proton.me>
2023-07-06 14:55:02 +0200

    products

--- src/utils/testData/products.ts
+++ src/utils/testData/products.ts
@@ -90,3 +90,55 @@ export const physiolac: Product = {
   location: 'M15E7T7 / R3LAIT3',
   availableStock: 1106
 }
+
+export const dolodent: Product = {
+  name: 'Dolodent solution 27g',
+  miniature:
+    'https://www.pharmacieagnespraden.com/img/tmp/product_mini_195_1.jpg',
+  img: 'https://www.pharmacieagnespraden.com/52664-large_default/dolodent-solution-gingivale-27-g.jpg',
+  categoryUuid: 'category-dents',
+  cip13: '3400921929201',
+  priceWithoutTax: 500,
+  percentTaxRate: 10,
+  location: 'DD02',
+  availableStock: 59
+}
+
+export const ultraLevure: Product = {
+  name: 'Ultra levure 200 mg 10 gélules',
+  miniature:
+    'https://www.pharmacieagnespraden.com/img/tmp/product_mini_198_1.jpg?time=1675901328',
+  img: 'https://www.pharmacieagnespraden.com/54697-large_default/ultra-levure-200-mg.jpg',
+  categoryUuid: 'category-diarrhee',
+  cip13: '3400922096612',
+  priceWithoutTax: 432,
+  percentTaxRate: 10,
+  location: 'C3',
+  availableStock: 36
+}
+
+export const anaca3Minceur: Product = {
+  name: 'Anaca3 Perte de Poids Infusion 24 Sachets',
+  miniature:
+    'https://www.pharmacieagnespraden.com/img/tmp/product_mini_198_1.jpg?time=1675901328',
+  img: 'https://www.pharmacieagnespraden.com/72000-large_default/anaca-3-minc-infus-perte-poids-24sach.jpg',
+  categoryUuid: 'category-minceur',
+  cip13: '3760007337185',
+  priceWithoutTax: 891,
+  percentTaxRate: 5.5,
+  location: '1D450',
+  availableStock: 36
+}
+
+export const chamomilla: Product = {
+  name: 'Chamomilla vulgaris 9CH suppositoires Boiron (poussée dentaire)',
+  miniature:
+    'https://www.pharmacieagnespraden.com/img/tmp/product_mini_198_1.jpg?time=1675901328',
+  img: 'https://www.pharmacieagnespraden.com/55-large_default/chamomilla-vulgaris-9-ch-boiron-12-suppositoires.jpg',
+  categoryUuid: 'category-dents',
+  cip13: '3400921924008',
+  priceWithoutTax: 627,
+  percentTaxRate: 10,
+  location: 'HOMEO-D',
+  availableStock: 1
+}
