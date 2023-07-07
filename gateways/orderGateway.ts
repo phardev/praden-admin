0e666ecde45abef6c8b6fac9f8a53e0f8ee11c75
gmevelec <gregory.mevelec@gmail.com>
2023-07-08 02:17:51 +0900

    Use real order gateway

--- gateways/orderGateway.ts
+++ gateways/orderGateway.ts
@@ -1,10 +1,13 @@
 import { InMemoryOrderGateway } from '@adapters/secondary/inMemoryOrderGateway'
 import * as orders from '@utils/testData/orders'
 import { RealDateProvider } from '@adapters/secondary/realDateProvider'
+import { RealOrderGateway } from '@adapters/secondary/realOrderGateway'
 
 const orderGateway = new InMemoryOrderGateway(new RealDateProvider())
 orderGateway.feedWith(...Object.values(orders))
 
 export const useOrderGateway = () => {
+  const { BACKEND_URL } = useRuntimeConfig()
+  return new RealOrderGateway(BACKEND_URL)
   return orderGateway
 }
