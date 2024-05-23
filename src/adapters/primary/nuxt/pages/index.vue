26cce5dd4133dcc952223fffd08fb30460387a13
gmevelec <gregory.mevelec@gmail.com>
2024-05-23 19:37:42 +0900

    Test env

--- src/adapters/primary/nuxt/pages/index.vue
+++ src/adapters/primary/nuxt/pages/index.vue
@@ -1,7 +1,12 @@
-<template lang="pug">
-div
+<template>
+  <div>
+    <p>Current Environment: {{ env }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({ layout: 'main' })
+const { ENV } = useRuntimeConfig().public
+console.log('Runtime Config ENV:', ENV) // Add this line to debug
+const env = ENV
 </script>
