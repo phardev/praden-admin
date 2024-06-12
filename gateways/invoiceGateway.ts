f821c1c5902c6edc8d0204f13b204f4b5b4d88da
gmevelec <gregory.mevelec@gmail.com>
2024-06-12 19:33:59 +0900

    Do not use real invoice gateway

--- gateways/invoiceGateway.ts
+++ gateways/invoiceGateway.ts
@@ -1,13 +1,10 @@
 import { InMemoryInvoiceGateway } from '@adapters/secondary/invoice-gateways/InMemoryInvoiceGateway'
 import { RealDateProvider } from '@adapters/secondary/date-providers/RealDateProvider'
-import { RealInvoiceGateway } from '@adapters/secondary/invoice-gateways/RealInvoiceGateway'
-import { isLocalEnv } from '@utils/env'
 
 export const useInvoiceGateway = () => {
-  if (isLocalEnv()) {
-    const invoiceGateway = new InMemoryInvoiceGateway(new RealDateProvider())
-    return invoiceGateway
-  }
-  const { BACKEND_URL } = useRuntimeConfig()
-  return new RealInvoiceGateway(BACKEND_URL)
+  // if (isLocalEnv()) {
+  return new InMemoryInvoiceGateway(new RealDateProvider())
+  // }
+  // const { BACKEND_URL } = useRuntimeConfig()
+  // return new RealInvoiceGateway(BACKEND_URL)
 }
