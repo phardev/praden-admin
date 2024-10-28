cc7cd06caa519213bc764db07c6013bfcce7afae
Piamias Victor <chocowalletdev@proton.me>
2024-10-28 11:08:04 +0100

    ft_table scroll

--- src/adapters/primary/nuxt/components/molecules/FtTable.vue
+++ src/adapters/primary/nuxt/components/molecules/FtTable.vue
@@ -8,49 +8,50 @@
   div(:class="{'mt-4': hasSearchSlot}")
     slot(name="search")
   .-mx-4.mt-10.ring-1.ring-light(class='sm:-mx-6 md:mx-0 md:rounded-lg')
-    table.min-w-full.divide-y.divide-light
-      thead.bg-contrast
-        tr
-          th(v-if="selectable" scope="col" class="relative w-12 px-6 sm:w-16 sm:px-8")
-            input(
-              :key="indeterminate || selectionIntersection.length === items.length"
-              type="checkbox"
-              class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-light text-colored focus:ring-colored sm:left-6"
-              :checked="indeterminate || (selectionIntersection.length === items.length && items.length > 0)"
-              :indeterminate="indeterminate"
-              @click.prevent="selectAll"
-            )
-          th.pl-4.pr-3.text-left.text-sm.font-semibold(
-            v-for="(header, headerIndex) in headers"
-            :key="headerIndex"
-            :class="[headerIndex === 0 ? 'pl-4 pr-3 sm:pl-6' : 'px-3 lg:table-cell', 'text-left text-sm font-semibold text-default py-3.5']"
-            scope='col'
-          ) {{ header.name }}
-      tbody(v-if="isLoading")
-        tr(v-for="i in 5" :key="i")
-          td(v-if="selectable" class="relative w-12 px-6 sm:w-16 sm:px-8")
-            USkeleton(class="h-4 w-4")
-          td(v-for="c in headers.length" :key="c")
-            USkeleton.my-4(class="h-4 w-[250px]")
-      tbody(v-else)
-        tr.table-line(
-          v-for='(item, index) in items'
-          :key='index'
-          @click="clicked(item)"
-        )
-          td.border-t.border-light(v-if="selectable" class="relative w-12 px-6 sm:w-16 sm:px-8")
-            input(
-              :key="selection.includes(item[itemKey])"
-              type="checkbox"
-              class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-light text-colored focus:ring-colored sm:left-6"
-              :checked="selection.includes(item[itemKey])"
-              @click.prevent="select(item)"
-            )
-          td.border-t.border-light.px-3.py-3.text-sm.text-contrast(
-            v-for="(header, headerIndex) in headers"
-            :key="headerIndex"
+    div(class="overflow-y-auto" style="max-height: 400px;")
+      table.min-w-full.divide-y.divide-light
+        thead.bg-contrast
+          tr
+            th(v-if="selectable" scope="col" class="relative w-12 px-6 sm:w-16 sm:px-8")
+              input(
+                :key="indeterminate || selectionIntersection.length === items.length"
+                type="checkbox"
+                class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-light text-colored focus:ring-colored sm:left-6"
+                :checked="indeterminate || (selectionIntersection.length === items.length && items.length > 0)"
+                :indeterminate="indeterminate"
+                @click.prevent="selectAll"
+              )
+            th.pl-4.pr-3.text-left.text-sm.font-semibold(
+              v-for="(header, headerIndex) in headers"
+              :key="headerIndex"
+              :class="[headerIndex === 0 ? 'pl-4 pr-3 sm:pl-6' : 'px-3 lg:table-cell', 'text-left text-sm font-semibold text-default py-3.5']"
+              scope='col'
+            ) {{ header.name }}
+        tbody(v-if="isLoading")
+          tr(v-for="i in 5" :key="i")
+            td(v-if="selectable" class="relative w-12 px-6 sm:w-16 sm:px-8")
+              USkeleton(class="h-4 w-4")
+            td(v-for="c in headers.length" :key="c")
+              USkeleton.my-4(class="h-4 w-[250px]")
+        tbody(v-else)
+          tr.table-line(
+            v-for='(item, index) in items'
+            :key='index'
+            @click="clicked(item)"
           )
-            slot(:name="header.value" :item="item") {{ getValue(item, header.value) }}
+            td.border-t.border-light(v-if="selectable" class="relative w-12 px-6 sm:w-16 sm:px-8")
+              input(
+                :key="selection.includes(item[itemKey])"
+                type="checkbox"
+                class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-light text-colored focus:ring-colored sm:left-6"
+                :checked="selection.includes(item[itemKey])"
+                @click.prevent="select(item)"
+              )
+            td.border-t.border-light.px-3.py-3.text-sm.text-contrast(
+              v-for="(header, headerIndex) in headers"
+              :key="headerIndex"
+            )
+              slot(:name="header.value" :item="item") {{ getValue(item, header.value) }}
 </template>
 
 <script lang="ts" setup>
