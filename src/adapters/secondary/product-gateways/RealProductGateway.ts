e8baaed516ed802c949600450cc25d1287840f6b
gmevelec <gregory.mevelec@gmail.com>
2024-11-05 23:26:53 +0900

    Update product gateway

--- src/adapters/secondary/product-gateways/RealProductGateway.ts
+++ src/adapters/secondary/product-gateways/RealProductGateway.ts
@@ -5,10 +5,49 @@ import axios from 'axios'
 import { CreateProductDTO } from '@core/usecases/product/product-creation/createProduct'
 import { UUID } from '@core/types/types'
 import { EditProductDTO } from '@core/usecases/product/product-edition/editProduct'
+import { Category } from '@core/entities/category'
 
 export class RealProductGateway extends RealGateway implements ProductGateway {
   constructor(url: string) {
     super(url)
+  }
+
+  async addProductsToCategory(
+    category: Category,
+    productUuids: Array<UUID>
+  ): Promise<Array<Product>> {
+    const res = await axios.post(
+      `${this.baseUrl}/products/add-category`,
+      {
+        categoryUuid: category.uuid,
+        productUuids: productUuids
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    return Promise.resolve(res.data.items)
+  }
+
+  async removeProductsFromCategory(
+    category: Category,
+    productUuids: Array<UUID>
+  ): Promise<Array<Product>> {
+    const res = await axios.post(
+      `${this.baseUrl}/products/remove-category`,
+      {
+        categoryUuid: category.uuid,
+        productUuids: productUuids
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    return Promise.resolve(res.data.items)
   }
 
   async getByCategoryUuid(uuid: UUID): Promise<Array<Product>> {
@@ -17,7 +56,6 @@ export class RealProductGateway extends RealGateway implements ProductGateway {
   }
 
   async batch(ean13s: Array<string>): Promise<Array<Product>> {
-    return Promise.resolve([])
     const res = await axios.post(`${this.baseUrl}/products/batch`, {
       headers: {
         'Content-Type': 'application/json'
